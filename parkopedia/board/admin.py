98d0a5484ce417472caffa6af03096f5ee168044
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2020-10-02 20:05:25 +0300

    Update admin.py

--- parkopedia/board/admin.py
+++ parkopedia/board/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from mapwidgets.widgets import GooglePointFieldWidget
+'''from mapwidgets.widgets import GooglePointFieldWidget
 
 class CityAdmin(admin.ModelAdmin):
     formfield_overrides = {
         models.PointField: {"widget": GooglePointFieldWidget}
-    }
+    }'''
