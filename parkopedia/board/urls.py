781bbe0df0929e94b790883678bc4e64309f2f5a
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2020-10-02 12:44:51 +0300

    Add files via upload

---
+++ parkopedia/board/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+
+from django.conf import settings
+
+urlpatterns = [
+    path('', views.index, name = "home" ),
+    #path("create/", create, name = "create-page"),
+
+] 
