25edd7ba83254fedde210993e74c8b94b6f5b0ff
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2020-10-02 22:09:01 +0300

    Update settings.py

--- parkopedia/parkopedia/settings.py
+++ parkopedia/parkopedia/settings.py
@@ -137,3 +137,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL='home'
+LOGIN_URL='login'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
