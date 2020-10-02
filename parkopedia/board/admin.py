af8485a286ab325dd2087267c743795dd82aacd3
Lacho <ZariProjects@users.noreply.github.com>
2020-10-02 18:58:41 +0300

    Update admin.py

--- parkopedia/board/admin.py
+++ parkopedia/board/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-
+"""
 from mapwidgets.widgets import GooglePointFieldWidget
 
 class CityAdmin(admin.ModelAdmin):
     formfield_overrides = {
-        models.PointField: {"widget": GooglePointFieldWidget}
-    }
+        models.PointField: {"widget": GooglePointFieldWidget}"""
