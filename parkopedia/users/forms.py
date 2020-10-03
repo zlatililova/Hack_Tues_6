6ccc33791769dca00a1ca40d2729dbf6a83bf564
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2020-10-03 22:06:03 +0300

    Update forms.py

--- parkopedia/users/forms.py
+++ parkopedia/users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import Profile, UserProfile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -10,13 +10,13 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-class UserUpdateForm(forms.Form):
+class UserUpdateForm(forms.ModelForm):
     email=forms.EmailField()
-    bio = forms.CharField(max_length=1000, required=False)
-    nickname = forms.CharField(max_length=250, required=False)
-    '''class Meta:
-        model = User
-        fields = ['username', 'email', 'bio', 'nickname']'''
+    first_name = forms.CharField(max_length=250, required=False)
+    last_name = forms.CharField(max_length=250, required=False)
+    class Meta:
+        model = UserProfile
+        fields = [ 'email', 'first_name', 'last_name']
 
 '''class ProfileUpdateForm(forms.ModelForm):
     class Meta:
