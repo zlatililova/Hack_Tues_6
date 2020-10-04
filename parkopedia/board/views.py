2ad78674eaa329bf3ce34f7f1a3c0a5f5e07faa5
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2020-10-04 11:43:18 +0300

    Update views.py

--- parkopedia/board/views.py
+++ parkopedia/board/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+from board.models import Post
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html') 
+    return render(request, 'index.html')
+
+def create(request):
+    if request.method == "POST":
+        obj = Post(address=request.POST.get("address"),
+                   rating=request.POST.get("rating"))
+        obj.save()
+        return render(request, "success.html")
+    return render(request, "create.html")
+ 
