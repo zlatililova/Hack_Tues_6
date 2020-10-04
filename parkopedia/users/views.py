98bd557183c1d253c28c9311dbfde7fd2676f107
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2020-10-04 11:48:36 +0300

    Update views.py

--- parkopedia/users/views.py
+++ parkopedia/users/views.py
@@ -18,19 +18,19 @@ def register(request):
 @login_required
 def profile(request):
     if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
+        u_form = UserUpdateForm(request.POST) #, instance=request.user
                                                                                             #'''p_form = ProfileUpdateForm(request.POST,
                                                                                              #                                       request.FILES,
                                                                                               #                                      instance=request.user.profile)
                                                                                     #             '''
         if u_form.is_valid():  #and p_form.is_valid()'''
             u_form.save()
-            email = u_form.cleaned_data['email']
-            first_name = u_form.cleaned_data['first_name']
-            last_name = u_form.cleaned_data['last_name']
+            #email = u_form.cleaned_data['email']
+            #first_name = u_form.cleaned_data['first_name']
+            #last_name = u_form.cleaned_data['last_name']
  
             return redirect('profile')
-            u_form = UserUpdateForm(instance=request.user)
+        u_form = UserUpdateForm()
             #p_form.save()
         messages.success(request, f'Your account has been updated!')
         return redirect('profile')
