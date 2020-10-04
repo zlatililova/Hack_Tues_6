db595c039ae7e3a052790558ffa6dbb36bc53bdc
Lacho <ZariProjects@users.noreply.github.com>
2020-10-04 20:46:54 +0300

    Update views.py

--- parkopedia/board/views.py
+++ parkopedia/board/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from board.models import Post
+from board.models import Post, MyPos
+from board.parking_search import nearest
 # Create your views here.
 
 def index(request):
@@ -13,3 +14,10 @@ def create(request):
         return render(request, "success.html")
     return render(request, "create.html")
  
+def find(request):
+    if request.method == "POST":
+        obj = MyPos(lat=request.POST.get("lat"),
+                    lon=request.POST.get("lon"))
+        obj.save()
+        return nearest(obj.lat, obj.lon)
+    return render(request, "badpage.html")
