b1f64d750b73db30068c05f1609777a7cbf9228e
NikolayLazarov <48582131+NikolayLazarov@users.noreply.github.com>
2020-10-04 10:12:26 +0300

    Create map_location.py

---
+++ map_location.py
@@ -0,0 +1,29 @@
+import googlemaps
+import pprint
+import time
+#from GoogleMapsAPIKey import get_my_key
+#from Auto_search import ready()
+
+#cords = ready()
+cords = '-33.8670522,151.195362'
+
+API_KEY = 'AIzaSyCndiwpn0s7MRo2qbhPbzhSxdODyPkFDBo'
+
+gmaps = googlemaps.Client(key = API_KEY)
+
+#define search
+
+places_result = gmaps.places_nearby(location = cords, radius = 200, open_now = False, type = 'parking')
+
+#loop through places_nearby
+for place in places_result['results']:
+	my_place_id = place['place_id']
+	
+	my_fields = ['formatted_address','name','opening_hours', 'formatted_phone_number','photo']
+	
+	#make request
+	place_details = gmaps.place(place_id = my_place_id, fields = my_fields)
+	
+	pprint.pprint(place_details)
+		
+	
