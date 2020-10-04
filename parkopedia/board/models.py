8be9d06d389755e98f75b530bc7083d154d1e060
Lacho <ZariProjects@users.noreply.github.com>
2020-10-04 20:47:10 +0300

    Update models.py

--- parkopedia/board/models.py
+++ parkopedia/board/models.py
@@ -5,3 +5,7 @@ class Post(models.Model):
     address = models.CharField(max_length=50, null=True, blank=True)
     rating = models.CharField(max_length=50)
     created = models.DateTimeField(auto_now_add=True)
+
+class MyPos(models.Model):
+    lat = models.FloatField(default=42.697450)
+    lon = models.FloatField(default=23.324280)
