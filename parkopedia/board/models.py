5d1f9a9b90c3b759443a4141bd1f4ded7cd1bc29
Lacho <ZariProjects@users.noreply.github.com>
2020-10-04 21:05:31 +0300

    modells.py

--- parkopedia/board/models.py
+++ parkopedia/board/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from mapbox_location_field.models import LocationField, AddressAutoHiddenField
 
 # Create your models here.
 class Post(models.Model):
@@ -9,3 +10,10 @@ class Post(models.Model):
 class MyPos(models.Model):
     lat = models.FloatField(default=42.697450)
     lon = models.FloatField(default=23.324280)
+
+class Location(models.Model):
+    location = LocationField(
+        map_attrs={"style": "mapbox://styles/mapbox/outdoors-v11", "marker_color": "blue", "center": (17.031645, 51.106715)})
+    create= models.DateTimeField(auto_now_add=True)
+    address = AddressAutoHiddenField()
+    
