2778ef7f8feca94588c35246c020d2d21a18cf83
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2020-10-04 18:45:44 +0300

    Update models.py

--- parkopedia/board/models.py
+++ parkopedia/board/models.py
@@ -1,7 +1,14 @@
 from django.db import models
-
+from geoposition.fields import GeopositionField
 # Create your models here.
 class Post(models.Model):
     address = models.CharField(max_length=50, null=True, blank=True)
     rating = models.CharField(max_length=50)
     created = models.DateTimeField(auto_now_add=True)
+
+
+
+
+class PointOfInterest(models.Model):
+    name = models.CharField(max_length=100)
+    position = GeopositionField()
