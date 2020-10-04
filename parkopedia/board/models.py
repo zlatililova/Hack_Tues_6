36dfecddb5496378fda2000464cd814f2eeb54be
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2020-10-04 20:22:35 +0300

    Update models.py

--- parkopedia/board/models.py
+++ parkopedia/board/models.py
@@ -1,14 +1,10 @@
 from django.db import models
-from geoposition.fields import GeopositionField
-# Create your models here.
-class Post(models.Model):
-    address = models.CharField(max_length=50, null=True, blank=True)
-    rating = models.CharField(max_length=50)
-    created = models.DateTimeField(auto_now_add=True)
+from mapbox_location_field.models import LocationField, AddressAutoHiddenField
 
 
-
-
-class PointOfInterest(models.Model):
-    name = models.CharField(max_length=100)
-    position = GeopositionField()
+class Location(models.Model):
+    location = LocationField(
+        map_attrs={"style": "mapbox://styles/mapbox/outdoors-v11", "marker_color": "blue", "center": (17.031645, 51.106715)})
+    create= models.DateTimeField(auto_now_add=True)
+    address = AddressAutoHiddenField()
+    
