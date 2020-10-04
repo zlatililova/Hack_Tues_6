b5508b4b79417538fcb7a2f3870d88c87b0d690c
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2020-10-04 11:48:39 +0300

    Update models.py

--- parkopedia/users/models.py
+++ parkopedia/users/models.py
@@ -27,5 +27,4 @@ class UserProfile(models.Model):
         first_name = models.CharField(max_length=250)
         last_name = models.CharField(max_length=250)
 
-        def __str__(self):
-            return self.user
+        
