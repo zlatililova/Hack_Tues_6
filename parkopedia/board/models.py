737ba3ab186b3656516f2b5195eb18fe3ad9e660
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2020-10-04 11:42:58 +0300

    Update models.py

--- parkopedia/board/models.py
+++ parkopedia/board/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    address = models.CharField(max_length=50, null=True, blank=True)
+    rating = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
