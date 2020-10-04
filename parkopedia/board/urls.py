e7f0d6483a9c616e05a6f7a15fe37894c935b50f
Lacho <ZariProjects@users.noreply.github.com>
2020-10-05 00:15:31 +0300

    Update urls.py

--- parkopedia/board/urls.py
+++ parkopedia/board/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import AddPlaceView
 from django.conf.urls.static import static
 from .views import AddPlaceView
 from django.conf import settings
