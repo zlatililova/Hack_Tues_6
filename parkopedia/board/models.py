d1efee5b79e2b475d2a24b60628dbbdafd608737
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2020-10-04 21:16:55 +0300

    Update models.py

--- parkopedia/board/models.py
+++ parkopedia/board/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from mapbox_location_field.models import LocationField, AddressAutoHiddenField
 
 # Create your models here.
 class Post(models.Model):
@@ -9,3 +10,8 @@ class Post(models.Model):
 class MyPos(models.Model):
     lat = models.FloatField(default=42.697450)
     lon = models.FloatField(default=23.324280)
+
+class Location(models.Model):
+    location = LocationField()
+    created = models.DateTimeField(auto_now_add=True)
+    address = AddressAutoHiddenField()
